9797ecf85b8e70ec507574d518d341657f176fd8
LeonEstrak <aniket2583@gmail.com>
2021-11-16 14:53:26 +0530

    Auth, Storage, Dynamo All Working

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import "./App.css";
+import Amplify, { Auth } from "aws-amplify";
+import awsconfig from "./aws-exports";
+
+import { useState } from "react";
+import { useEffect } from "react";
+
+import { Button } from "@material-ui/core";
+import SongList from "./components/SongList";
+
+import SignIn from "./components/SignIn";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+Amplify.configure(awsconfig);
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const assessLoggedInState = () => {
+    Auth.currentAuthenticatedUser()
+      .then((sess) => {
+        console.log("logged in");
+        setLoggedIn(true);
+      })
+      .catch(() => {
+        console.log("not logged in");
+        setLoggedIn(false);
+      });
+  };
+  useEffect(() => {
+    assessLoggedInState();
+  }, []);
+
+  const signOut = async () => {
+    try {
+      await Auth.signOut();
+      setLoggedIn(false);
+    } catch (error) {
+      console.log("error signing out: ", error);
+    }
+  };
+
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h2>
+            Cloud Project
+            <p style={{ fontSize: 14 }}>
+              {" "}
+              Made by Aniket, Swattik, Sahasrabdi, Ritu{" "}
+            </p>
+          </h2>
+          {loggedIn ? (
+            <Button onClick={signOut} variant="contained" color="primary">
+              Log Out
+            </Button>
+          ) : (
+            <Link to="/signin">
+              <Button variant="contained" color="primary">
+                Log In
+              </Button>
+            </Link>
+          )}
+        </header>
+        <Route exact path="/">
+          <SongList />
+        </Route>
+        <Route path="/signin">
+          <SignIn onSignin={assessLoggedInState} />
+        </Route>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/SignIn/index.jsx
@@ -0,0 +1,86 @@
+//@ts-check
+import React, { useState } from "react";
+import { Auth } from "aws-amplify";
+import { Button, TextField } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+
+const Signin = ({ onSignin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [authCode, setAuthCode] = useState("");
+  const [showAuthCode, setShowAuthCode] = useState(false);
+  const history = useHistory();
+
+  const signIn = async () => {
+    try {
+      await Auth.signIn(username, password);
+      history.push("/");
+      onSignin();
+    } catch (error) {
+      console.log("error signing in", error);
+    }
+  };
+
+  const signUp = async () => {
+    try {
+      await Auth.signUp(username, password);
+      console.log("success");
+      setShowAuthCode(true);
+    } catch (error) {
+      console.log("error signing up", error);
+    }
+  };
+
+  const confirmSignUp = async () => {
+    try {
+      await Auth.confirmSignUp(username, authCode);
+      console.log("success Confirmation");
+      setShowAuthCode(false);
+    } catch (error) {
+      console.log("error signing up", error);
+    }
+  };
+  return (
+    <div className="login">
+      <TextField
+        id="username"
+        label="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      {!showAuthCode ? (
+        <TextField
+          id="password"
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      ) : (
+        <TextField
+          id="authcode"
+          label="authcode"
+          value={authCode}
+          onChange={(e) => setAuthCode(e.target.value)}
+        />
+      )}
+
+      {showAuthCode ? (
+        <Button id="signinButton" color="primary" onClick={confirmSignUp}>
+          Confirm Sign Up
+        </Button>
+      ) : (
+        <div style={{ display: "contents" }}>
+          <Button id="signinButton" color="primary" onClick={signIn}>
+            Sign In
+          </Button>
+          <Button id="signinButton" color="primary" onClick={signUp}>
+            Sign Up
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Signin;
